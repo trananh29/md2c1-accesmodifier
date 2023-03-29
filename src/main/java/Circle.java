5767559281b4925edc2a4d4322cc40fbade0b7b4
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-03-29 14:20:37 +0700

    xong

---
+++ src/main/java/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+    private  double radius =1.0;
+    private String colour ="red";
+    //Constructor
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+}
---
+++ src/main/java/TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c1= new Circle();
+        c1.getArea();
+        c1.getRadius();
+    }
+}
